ee57f731d49bc173d410912504fc1b59f7bc056d
root <root@ip-10-4-4-218.us-west-2.compute.internal>
2017-09-17 06:03:49 +0000

    test

---
+++ outputs.tf
@@ -0,0 +1,48 @@
+output "private_subnets" {
+  value = ["${aws_subnet.private.*.id}"]
+}
+
+output "public_subnets" {
+  value = ["${aws_subnet.public.*.id}"]
+}
+
+output "vpc_id" {
+  value = "${aws_vpc.mod.id}"
+}
+
+output "vpc_cidr_block" {
+  value = "${aws_vpc.mod.cidr_block}"
+}
+
+output "public_route_table_ids" {
+  value = ["${aws_route_table.public.*.id}"]
+}
+
+output "private_route_table_ids" {
+  value = ["${aws_route_table.private.*.id}"]
+}
+
+output "default_security_group_id" {
+  value = "${aws_vpc.mod.default_security_group_id}"
+}
+
+output "nat_eips" {
+  value = ["${aws_eip.nateip.*.id}"]
+}
+
+output "nat_eips_public_ips" {
+  value = ["${aws_eip.nateip.*.public_ip}"]
+}
+
+output "natgw_ids" {
+  value = ["${aws_nat_gateway.natgw.*.id}"]
+}
+
+output "igw_id" {
+  value = "${aws_internet_gateway.mod.id}"
+}
+
+output "default_network_acl_id" {
+  value = "${aws_vpc.mod.default_network_acl_id}"
+}
+
---
+++ variables.tf
@@ -0,0 +1,65 @@
+variable "name" {
+  description = "Name to be used on all the resources as identifier"
+  default     = ""
+}
+
+variable "cidr" {
+  description = "The CIDR block for the VPC"
+  default     = ""
+}
+
+variable "public_subnets" {
+  description = "A list of public subnets inside the VPC."
+  default     = []
+}
+
+variable "private_subnets" {
+  description = "A list of private subnets inside the VPC."
+  default     = []
+}
+
+variable "azs" {
+  description = "A list of Availability zones in the region"
+  default     = []
+}
+
+variable "enable_dns_hostnames" {
+  description = "should be true if you want to use private DNS within the VPC"
+  default     = false
+}
+
+variable "enable_dns_support" {
+  description = "should be true if you want to use private DNS within the VPC"
+  default     = false
+}
+
+variable "enable_nat_gateway" {
+  description = "should be true if you want to provision NAT Gateways for each of your private networks"
+  default     = false
+}
+
+variable "single_nat_gateway" {
+  description = "should be true if you want to provision a single shared NAT Gateway across all of your private networks"
+  default     = false
+}
+
+variable "map_public_ip_on_launch" {
+  description = "should be false if you do not want to auto-assign public IP on launch"
+  default     = true
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  default     = {}
+}
+
+variable "public_subnet_tags" {
+  description = "Additional tags for the public subnets"
+  default     = {}
+}
+
+variable "private_subnet_tags" {
+  description = "Additional tags for the public subnets"
+  default     = {}
+}
+
