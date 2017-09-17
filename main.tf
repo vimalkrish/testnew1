ef76d2d4bd2b396cbc27c8461687bebf751843e4
vimalkrish <linuxvimal@gmail.com>
2017-09-17 12:02:09 +0530

    Update main.tf
    test

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 resource "aws_vpc" "mod" {
   cidr_block           = "${var.cidr}"
-  instance_tenancy     = "${var.instance_tenancy}"
   enable_dns_hostnames = "${var.enable_dns_hostnames}"
   enable_dns_support   = "${var.enable_dns_support}"
 
